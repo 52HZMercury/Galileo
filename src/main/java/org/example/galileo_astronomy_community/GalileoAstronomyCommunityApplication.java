6fb98fee391fa78224cdca3d671b0ca156552b97
52HZMercury <chenning_william@163.com>
2024-09-19 16:52:24 +0800

    fist push

---
+++ src/main/java/org/example/galileo_astronomy_community/GalileoAstronomyCommunityApplication.java
@@ -0,0 +1,13 @@
+package org.example.galileo_astronomy_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalileoAstronomyCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalileoAstronomyCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/galileo_astronomy_community/GalileoAstronomyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.galileo_astronomy_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalileoAstronomyCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
